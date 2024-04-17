945c509f9d66fefe70e6d474921ec580336564ea
Dewald Haasbroek <dewald.haasbroek@agrico.co.za>
2024-04-17 21:14:01 +0200

    End of topic one , also added a map of function pointers

--- MerklTradeingApp/main.cpp
+++ MerklTradeingApp/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 
 void printMenu () {
     std::cout << "+ ----------------------------- +" << std::endl;
@@ -15,34 +16,55 @@ void printMenu () {
     std::cout << std::endl;
 }
 
-void handleUserChoice(int userChoice) {
-    switch (userChoice) {
-        case 1:
-            std::cout << "Help: " << std::endl;
-            break;
-        case 2:
-            std::cout << "Exchange rates: " << std::endl;
-            break;
-        case 3:
-            std::cout << "Make a offer: " << std::endl;
-            break;
-        case 4:
-            std::cout << "Make a bid: " << std::endl;
-            break;
-        case 5:
-            std::cout << "Print wallet: " << std::endl;
-            break;
-        case 6:
-            std::cout << "Next time slice: " << std::endl;
-            break;
-        case 7:
-            std::cout << "Goodbye " << std::endl;
-            break;
-        default:
-            std::cout << "Invalid choice" << std::endl;
-            break;
-    }
-    std::cout << std::endl;
+int getUserChoice() {
+    int userChoice;
+    std::cout << "Please Select a number: ";
+    std::cin >> userChoice;
+    return userChoice;
+}
+
+void printHelp() {
+    std::cout << "Help - your aim is to make money." << std::endl;
+    std::cout << "Analyse the market and make bids" << std::endl;
+    std::cout << "and offers. " << std::endl;
+}
+
+void printExchangeRates() {
+    std::cout << "Exchange rates: " << std::endl;
+}
+
+void makeOffer() {
+    std::cout << "Make a offer: " << std::endl;
+}
+
+void placeBid() {
+    std::cout << "Make a bid: " << std::endl;
+}
+
+void printWallet() {
+    std::cout << "Print wallet: " << std::endl;
+}
+
+void advanceToNextTimeSlice() {
+    std::cout << "Next time slice: " << std::endl;
+}
+
+void exitProgram() {
+    std::cout << "Goodbye " << std::endl;
+}
+
+//Build Map of function pointers
+std::map<int,void(*)()> userChoiceHandler () {
+    std::map<int, void(*)()> outputMap;
+    outputMap[0] = printMenu;
+    outputMap[1] = printHelp;
+    outputMap[2] = printExchangeRates;
+    outputMap[3] = makeOffer;
+    outputMap[4] = placeBid;
+    outputMap[5] = printWallet;
+    outputMap[6] = advanceToNextTimeSlice;
+    outputMap[7] = exitProgram;
+    return outputMap;
 }
 
 // Main function
@@ -53,13 +75,9 @@ int main () {
     while(userChoice != 7) {
 
         printMenu();
+        userChoice = getUserChoice();
+        userChoiceHandler()[userChoice]();
 
-        std::cout << "Please Select a number: ";
-        std::cout << std::endl;
-
-        std::cin >> userChoice;
-
-        handleUserChoice(userChoice);
     }
 
 
--- Playground/test.cpp
+++ Playground/test.cpp
@@ -2,5 +2,6 @@
 
 int main () {
     std::cout << "Hello World" << std::endl;
-    return 0;
+    std::cout << "This was added by vim" << std::endl;
+	return 0;
 }
